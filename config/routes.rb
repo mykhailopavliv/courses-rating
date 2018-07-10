9d06310ea14217188166fc6d603a043a718eae4a
yuriirogulya <yuriirogulya@gmail.com>
2018-07-10 23:29:12 +0300

    changes views from erb to haml

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,4 @@ Rails.application.routes.draw do
   root 'home#index'
   devise_for :users
   resources :courses
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
