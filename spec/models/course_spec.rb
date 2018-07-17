c8faf3d13b6d5ed80934018704303e3bc3900fc7
Lomey <bohdanlomey@gmail.com>
2018-07-18 02:41:29 +0300

    Added logo has_one_attached relation example, renamed courses_spec --> courses_controller_spec

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -1,13 +1,17 @@
 require 'rails_helper'
 
 describe Course, type: :model do
-  describe 'relations' do
+  let(:course) { create(:course) }
+
+  describe 'Relations' do
     it { is_expected.to belong_to(:organization) }
     it { is_expected.to belong_to(:city) }
     it { is_expected.to have_many(:reviews) }
-  end
 
-  let(:course) { create(:course) }
+    context 'has_one_attached :logo' do
+      it { expect { course.logo }.to_not raise_error }
+    end
+  end
 
   describe 'Upload logo' do
     let(:file_full_path) { Rails.root.join('app/assets/images/pivorak logo/pivorak_logo.png') }
