56861f65a4f5eb0e97064fafd54f7bf743f8e524
Lomey <bohdanlomey@gmail.com>
2018-07-23 18:08:03 +0300

    Fixed reviews_controller after merging

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,10 +1,6 @@
 class ReviewsController < ApplicationController
   def create
-<<<<<<< HEAD
-    @review        = @course.reviews.build(review_params)
-=======
-    @review = course.reviews.build(review_params)
->>>>>>> 483579c5355a987944b02b5192ba7c0508c6e341
+    @review        = course.reviews.build(review_params)
     @review.author = current_user
     authorize @review
 
@@ -17,13 +13,7 @@ class ReviewsController < ApplicationController
   end
 
   def destroy
-<<<<<<< HEAD
-    @review = @course.reviews.find(params[:id])
-    authorize @review
-
-=======
-    @review = Review.find(params[:id])
->>>>>>> 483579c5355a987944b02b5192ba7c0508c6e341
+    @review = authorize course.reviews.find(params[:id])
     @review.destroy
     redirect_back(fallback_location: course_path(course))
     flash[:notice] = t('.destroyed')
@@ -32,12 +22,7 @@ class ReviewsController < ApplicationController
   private
 
   def review_params
-<<<<<<< HEAD
     permitted_attributes(Review)
-    # params.require(:review).permit(:text, :user_id)
-=======
-    params.require(:review).permit(:text)
->>>>>>> 483579c5355a987944b02b5192ba7c0508c6e341
   end
 
   def course
