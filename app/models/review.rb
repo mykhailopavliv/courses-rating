124637c3faa225c40c01ffd953cf117b610a0358
yuriirogulya <yuriirogulya@gmail.com>
2018-07-24 22:46:29 +0300

    added presense validation for review text

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,8 @@ class Review < ApplicationRecord
   belongs_to :course
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
 
+  validates :text, presence: true
+
   scope :published,    -> { where(published: true) }
   scope :unpublished,  -> { where(published: false) }
 end
