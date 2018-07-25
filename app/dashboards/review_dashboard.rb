609fdac4d34947974c1f30f2630930d2282b91e4
yuriirogulya <yuriirogulya@gmail.com>
2018-07-25 15:04:25 +0300

    change dashboards output

--- app/dashboards/review_dashboard.rb
+++ app/dashboards/review_dashboard.rb
@@ -1,12 +1,6 @@
 require "administrate/base_dashboard"
 
 class ReviewDashboard < Administrate::BaseDashboard
-  # ATTRIBUTE_TYPES
-  # a hash that describes the type of each of the model's fields.
-  #
-  # Each different type represents an Administrate::Field object,
-  # which determines how the attribute is displayed
-  # on pages throughout the dashboard.
   ATTRIBUTE_TYPES = {
     course: Field::BelongsTo,
     author: Field::BelongsTo.with_options(class_name: "User"),
@@ -15,23 +9,16 @@ class ReviewDashboard < Administrate::BaseDashboard
     author_id: Field::Number,
     created_at: Field::DateTime,
     updated_at: Field::DateTime,
-    published: Field::Boolean,
+    published: Field::Boolean
   }.freeze
 
-  # COLLECTION_ATTRIBUTES
-  # an array of attributes that will be displayed on the model's index page.
-  #
-  # By default, it's limited to four items to reduce clutter on index pages.
-  # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = [
     :course,
     :author,
-    :id,
     :text,
+    :published
   ].freeze
 
-  # SHOW_PAGE_ATTRIBUTES
-  # an array of attributes that will be displayed on the model's show page.
   SHOW_PAGE_ATTRIBUTES = [
     :course,
     :author,
@@ -40,24 +27,14 @@ class ReviewDashboard < Administrate::BaseDashboard
     :author_id,
     :created_at,
     :updated_at,
-    :published,
+    :published
   ].freeze
 
-  # FORM_ATTRIBUTES
-  # an array of attributes that will be displayed
-  # on the model's form (`new` and `edit`) pages.
   FORM_ATTRIBUTES = [
     :course,
     :author,
     :text,
     :author_id,
-    :published,
+    :published
   ].freeze
-
-  # Overwrite this method to customize how reviews are displayed
-  # across all pages of the admin dashboard.
-  #
-  # def display_resource(review)
-  #   "Review ##{review.id}"
-  # end
 end
--- app/dashboards/user_dashboard.rb
+++ app/dashboards/user_dashboard.rb
@@ -1,12 +1,6 @@
 require "administrate/base_dashboard"
 
 class UserDashboard < Administrate::BaseDashboard
-  # ATTRIBUTE_TYPES
-  # a hash that describes the type of each of the model's fields.
-  #
-  # Each different type represents an Administrate::Field object,
-  # which determines how the attribute is displayed
-  # on pages throughout the dashboard.
   ATTRIBUTE_TYPES = {
     courses: Field::HasMany,
     reviews: Field::HasMany,
@@ -26,21 +20,18 @@ class UserDashboard < Administrate::BaseDashboard
     last_sign_in_ip: Field::String.with_options(searchable: false),
     created_at: Field::DateTime,
     updated_at: Field::DateTime,
-    url: Field::String,
+    url: Field::String
   }.freeze
 
-  # COLLECTION_ATTRIBUTES
-  # an array of attributes that will be displayed on the model's index page.
-  #
-  # By default, it's limited to four items to reduce clutter on index pages.
-  # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = [
+    :first_name,
+    :last_name,
+    :role,
+    :email,
     :courses,
-    :reviews,
+    :reviews
   ].freeze
 
-  # SHOW_PAGE_ATTRIBUTES
-  # an array of attributes that will be displayed on the model's show page.
   SHOW_PAGE_ATTRIBUTES = [
     :courses,
     :reviews,
@@ -60,12 +51,9 @@ class UserDashboard < Administrate::BaseDashboard
     :last_sign_in_ip,
     :created_at,
     :updated_at,
-    :url,
+    :url
   ].freeze
 
-  # FORM_ATTRIBUTES
-  # an array of attributes that will be displayed
-  # on the model's form (`new` and `edit`) pages.
   FORM_ATTRIBUTES = [
     :courses,
     :reviews,
@@ -82,13 +70,10 @@ class UserDashboard < Administrate::BaseDashboard
     :last_sign_in_at,
     :current_sign_in_ip,
     :last_sign_in_ip,
-    :url,
+    :url
   ].freeze
 
-  # Overwrite this method to customize how users are displayed
-  # across all pages of the admin dashboard.
-  #
-  # def display_resource(user)
-  #   "User ##{user.id}"
-  # end
+  def display_resource(user)
+    "#{user.first_name} #{user.last_name}"
+  end
 end
