766171b612796c13e848d043e8b418fac32433d5
yuriirogulya <yuriirogulya@gmail.com>
2018-07-27 23:06:06 +0300

    changed rating output on course block

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,24 @@ module ApplicationHelper
 
     yield(presenter) if block_given?
   end
+
+  def rates_count(course)
+    course.rates('rating').count
+  end
+
+  def blank_stars(course)
+    5 - course.average('rating')&.avg.to_i
+  end
+
+  def filled_stars(course)
+    course.average('rating')&.avg.to_i
+  end
+
+  def average_rating(course)
+    course.average('rating')&.avg
+  end
+
+  def reviews_count(course)
+    rates_count(course) if rates_count(course) >= 1
+  end
 end
